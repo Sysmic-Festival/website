b5f24889618d0c754f5e74944235a1a4ebe502c7
Emile Cornamusaz <emile.cornamusaz@epfl.ch>
2024-10-14 21:52:08 +0200

    update artists

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -22,67 +22,60 @@ function backdropClicked(e) {
                 <div id="modal-backdrop" @click="backdropClicked"></div>
                 <section id="modal">
                     <div id="top-row">
-                        <div v-if="state.currentPlayer === 'Von Bikräv'">
-                            <iframe class="player-ui" width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1506591934&color=%237f5af0&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/user-170567008" title="Mismic22" target="_blank" style="color: #cccccc; text-decoration: none;">Mismic22</a> · <a href="https://soundcloud.com/user-170567008/sets/jewel-usain" title="Von Bikrav" target="_blank" style="color: #cccccc; text-decoration: none;">Von Bikrav</a></div>                        
+                        <div v-if="state.currentPlayer === 'player1'">
+                            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1E4xmFTrdt6YVF?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>                        
                         </div>
-                        <div v-if="state.currentPlayer === 'Fluffy Machine'">
-                            <iframe class="player-ui" width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1506590581&color=%237f5af0&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/user-170567008" title="Mismic22" target="_blank" style="color: #cccccc; text-decoration: none;">Mismic22</a> · <a href="https://soundcloud.com/user-170567008/sets/fluffy-machine" title="Fluffy Machine" target="_blank" style="color: #cccccc; text-decoration: none;">Fluffy Machine</a></div>                        
+                        <div v-if="state.currentPlayer === 'player2'">
+                            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DZ06evO097phG?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>                        
                         </div>
-                        <div v-if="state.currentPlayer === 'MEL'">
-                            <iframe class="player-ui" width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1510700551&color=%237f5af0&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/user-170567008" title="Mismic22" target="_blank" style="color: #cccccc; text-decoration: none;">Mismic22</a> · <a href="https://soundcloud.com/user-170567008/sets/mel" title="MEL" target="_blank" style="color: #cccccc; text-decoration: none;">MEL</a></div>                        
+                        <div v-if="state.currentPlayer === 'player3'">
+                            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DZ06evO3UtSB2?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                         </div>
-                        <div v-if="state.currentPlayer === 'High Salute b2b Radikal Vibration'">
-                            <iframe class="player-ui" width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1506584944&color=%237f5af0&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/user-170567008" title="Mismic22" target="_blank" style="color: #cccccc; text-decoration: none;">Mismic22</a> · <a href="https://soundcloud.com/user-170567008/sets/high-salute-b2b-radikal" title="High Salute b2b Radikal Vibration" target="_blank" style="color: #cccccc; text-decoration: none;">High Salute b2b Radikal Vibration</a></div>                        
+                        <div v-if="state.currentPlayer === 'player4'">
+                            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/7INLeFGVmRkHQcukM4nuKt?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                         </div>
 
-                        <div v-if="state.currentPlayer === 'Roots Zombie'">
-                            <iframe class="player-ui" width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1506586531&color=%237f5af0&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/user-170567008" title="Mismic22" target="_blank" style="color: #cccccc; text-decoration: none;">Mismic22</a> · <a href="https://soundcloud.com/user-170567008/sets/roots-zombie" title="Roots Zombie" target="_blank" style="color: #cccccc; text-decoration: none;">Roots Zombie</a></div>
+                        <div v-if="state.currentPlayer === 'player5'">
+                            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1E4v9UkbXR0w6B?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                         </div>
-                        <div v-if="state.currentPlayer === 'Loudjack'">
-                            <iframe class="player-ui" width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1506593185&color=%237f5af0&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/user-170567008" title="Mismic22" target="_blank" style="color: #cccccc; text-decoration: none;">Mismic22</a> · <a href="https://soundcloud.com/user-170567008/sets/tremplin" title="Loudjack" target="_blank" style="color: #cccccc; text-decoration: none;">Loudjack</a></div>                        
+                        <div v-if="state.currentPlayer === 'player6'">
+                            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1E4vdp4avdwNly?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                         </div>
-                        <div v-if="state.currentPlayer === 'PlanH150'">
-                            <iframe class="player-ui" width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1510646521&color=%237f5af0&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/plan-h150" title="PLAN H150" target="_blank" style="color: #cccccc; text-decoration: none;">PLAN H150</a> · <a href="https://soundcloud.com/plan-h150/sets/live-swisstech-village-2022" title="Live @ Swisstech Village 2022 - Part 1" target="_blank" style="color: #cccccc; text-decoration: none;">Live @ Swisstech Village 2022 - Part 1</a></div>
-                        </div>
-                        <div v-if="state.currentPlayer === 'Jewel Usain'">
-                            <iframe class="player-ui" style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1DZ06evO1aKR0s?utm_source=generator&theme=0" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>                  
+                        <div v-if="state.currentPlayer === 'player7'">
+                            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1E4z053PtoHKGK?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
                         </div>
 
                     </div>
                     <div id="bottom-row">
-                        <div @click="state.currentPlayer = 'Von Bikräv'" class="artistButton">
-                            <div><img class="artist-img" src="@/assets/images/artists/von-bikrav.jpg"></div>
-                            <p>Von Bikräv</p>
+                        <div @click="state.currentPlayer = 'player1'" class="artistButton"> 
+                            <div><img class="artist-img" src="@/assets/images/artists/parsimony.jpg"></div>
+                            <p>Parsimony</p>
                         </div>
-                        <div @click="state.currentPlayer = 'Fluffy Machine'" class="artistButton">
-                            <div><img class="artist-img" src="@/assets/images/artists/fluffy-machine.jpg"></div>
-                            <p>Fluffy Machine</p>
+                        <div @click="state.currentPlayer = 'player2'" class="artistButton">
+                            <div><img class="artist-img" src="@/assets/images/artists/sheng.jpg"></div>
+                            <p>Sheng</p>
                         </div>
-                        <div @click="state.currentPlayer = 'MEL'" class="artistButton">
-                            <div><img class="artist-img" src="@/assets/images/artists/mel.jpg"></div>
-                            <p>MEL</p>
+                        <div @click="state.currentPlayer = 'player3'" class="artistButton">
+                            <div><img class="artist-img" src="@/assets/images/artists/lebard.webp"></div>
+                            <p>Le Bard</p>
                         </div>
-                        <div @click="state.currentPlayer = 'High Salute b2b Radikal Vibration'" class="artistButton">
-                            <div><img class="artist-img" src="@/assets/images/artists/hsb2brv.jpg"></div>
-                            <p style="font-size:10pt">High Salute b2b Radikal Vibration</p>
+                        <div @click="state.currentPlayer = 'player4'" class="artistButton">
+                            <div><img class="artist-img" src="@/assets/images/artists/lakeouaaah.png"></div>
+                            <p style="font-size:10pt">Lake & OUAAHH</p>
                         </div>
                     </div>
                     <div id="bottom-row">
-                        <div @click="state.currentPlayer = 'Roots Zombie'" class="artistButton">
-                            <div><img class="artist-img" src="@/assets/images/artists/roots-zombie.jpg"></div>
-                            <p>Roots Zombie</p>
+                        <div @click="state.currentPlayer = 'player5'" class="artistButton">
+                            <div><img class="artist-img" src="@/assets/images/artists/neuns.jpg"></div>
+                            <p>Neun's</p>
                         </div>
-                        <div @click="state.currentPlayer = 'Loudjack'" class="artistButton">
-                            <div><img class="artist-img" src="@/assets/images/artists/loudjack.jpg"></div>
-                            <p>Loudjack</p>
+                        <div @click="state.currentPlayer = 'player6'" class="artistButton">
+                            <div><img class="artist-img" src="@/assets/images/artists/yenkov.jpg"></div>
+                            <p>Yenkov</p>
                         </div>
-                        <div @click="state.currentPlayer = 'PlanH150'" class="artistButton">
-                            <div><img class="artist-img" src="@/assets/images/artists/planh150.jpg"></div>
-                            <p>PlanH150</p>
-                        </div>
-                        <div @click="state.currentPlayer = 'Jewel Usain'" class="artistButton">
-                            <div><img class="artist-img" src="@/assets/images/artists/jewel-usain.jpg"></div>
-                            <p>Jewel Usain</p>
+                        <div @click="state.currentPlayer = 'player7'" class="artistButton">
+                            <div><img class="artist-img" src="@/assets/images/artists/dinaMDN.jpg"></div>
+                            <p>Dina MDN</p>
                         </div>
                     </div>
                 </section>
