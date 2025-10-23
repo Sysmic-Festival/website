03fb47a14d1949ae0c73eb873622c52d39c228f8
jidae-hub <nielkf@gmail.com>
2025-10-23 17:28:02 +0200

    Déplacement de la rubrique se loger dans infos pratiques

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -46,17 +46,6 @@ import FAQRow from "../components/FAQRow.vue";
             <br /><br />
             Cette année, la 29ème édition aura lieu le vendredi 7 novembre.
           </p>
-          <h2 id="se-loger" class="h2-primary-background">Se loger</h2>
-          <p>
-            Pour te loger durant le festival, nous te recommendons notre partenaire
-            le <i>SwissTech Hotel</i>, qui proposera des chambres à tarif fixé pour nos festivaliers !
-            <br /><br />
-            Situé en plein coeur du campus, à quelques pas de l'entrée du festival, le
-            <i>SwissTech Hotel</i> te permet de profiter de ton séjour dans le confort et la simplicité.
-            <br /><br />
-            Si tu cherches un hébergement pendant la soirée, on te recommande d'aller 
-            faire un tour sur leur <a href="https://www.swisstech-hotel.com/fr/">site web</a>.
-          </p>
         </section>
       </div>
       <div class="slider">
@@ -207,6 +196,21 @@ import FAQRow from "../components/FAQRow.vue";
           </p>
         </section>
       </div>
+      <div class="container">
+        <section>
+          <h2 id="se-loger" class="h2-primary-background">Se loger</h2>
+          <p>
+            Pour te loger durant le festival, nous te recommendons notre partenaire
+            le <i>SwissTech Hotel</i>, qui proposera des chambres à tarif fixé pour nos festivaliers !
+            <br /><br />
+            Situé en plein coeur du campus, à quelques pas de l'entrée du festival, le
+            <i>SwissTech Hotel</i> te permet de profiter de ton séjour dans le confort et la simplicité.
+            <br /><br />
+            Si tu cherches un hébergement pendant la soirée, on te recommande d'aller 
+            faire un tour sur leur <a href="https://www.swisstech-hotel.com/fr/">site web</a>.
+          </p>
+        </section>
+      </div>
       <hr class="mt-hr" style="max-width: 150px" />
       <hr class="mt-hr" style="max-width: 200px" />
       <hr class="mt-hr-blue" />
@@ -220,7 +224,7 @@ import FAQRow from "../components/FAQRow.vue";
           <RouterLink
             to="/faq#micros"
             style="color: var(--white); text-decoration: underline"
-            >ici</RouterLink
+            > ici</RouterLink
           >
           !
         </p>
